e8105697dfbe3cd412a4fd27d62890c58412ff15
Enzo322 <enzo_correa@estudante.sc.senai.br>
2022-11-01 12:31:39 -0300

    1/11 manhã

---
+++ Malwee-main/frontend/src/app/model-produto/model-produto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-produto',
+  templateUrl: './model-produto.component.html',
+  styleUrls: ['./model-produto.component.scss']
+})
+export class ModelProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
